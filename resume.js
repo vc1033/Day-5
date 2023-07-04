547689d7fe2f419ca18814152beeddccf8a15c93
vc1033 <vaishnavc9745@gmail.com>
2023-07-04 15:32:46 +0530

    resume in JSON format

---
+++ resume.js
@@ -0,0 +1,64 @@
+
+{
+  "name": "Vaishnav C",
+  "email": "vaishnavc@gmail.com",
+  "phone": "+91 8921216682",
+  "address": "irinjalakuda , Thrsissur , Kerala",
+  "summary": "Result-oriented Software Developer with two years ofexperience working as a Java SpringBoot Developer inthe BFSI (Banking, Financial Services, and Insurance)industry, working at TCS (Tata Consultancy Services).Proven expertise in developing and delivering high-quality software solutions to meet complex businessrequirements. Recently completed a three-month MERNStack Certification, expanding proficiency in full-stackweb development.",
+  "education": [
+    {
+      "degree": "Bachelor of Technology Mechanical",
+      "university": "KTU",
+      "location": "Irinjalakuda , thrissur",
+      "year": "2021"
+    }
+  ],
+  "experience" : [
+    {
+      "position": "Assistant System Engineer - Trainee",
+      "company": "TCS",
+      "location": "Kochi",
+      "startDate": "2021-08",
+      "endDate": "2021-10",
+      "responsibilities": 
+        "Joined the organisation as an ASE-Trainee , clearedthe DCA exam and got promoted to System engineerwithin a span of three months."
+    },
+    {
+        "position": "System Engineer",
+        "company": "TCS",
+        "location": "Kochi",
+        "startDate": "2021-10",
+        "endDate": "2023",
+      "responsibilities": [
+        "Developed , maintained and carried out unit testing forJava codes integrated with SpringBoot and databaseschemas for different banking applications and carriedout deployment in E1 , E2 and E3 environment usingGit,CI/CD and Jenkins.",
+        "Created ELK alerts to trigger Slack and Service Nowgroup alerts and to consolidate it's corresponding logsfor multiple banking applications.",
+        "Integrated with team and carried out IncidentManagement operations for ITSM without undergoingSLA breach",
+        
+      ]
+    }
+  ],
+  "skills": [
+    "Java",
+    "SpringBoot",
+    "JavaScript",
+    "React.js",
+    "Node.js",
+    "HTML",
+    "CSS",
+    "Git",
+  ],
+  "languages": [
+    {
+      "language": "Malayalam",
+      "proficiency": "Native"
+    },
+    {
+      "language": "English",
+      "proficiency": "Fluent"
+    },
+    {
+        "language": "Hindi",
+        "proficiency": "Fluent"
+      }
+  ]
+}
